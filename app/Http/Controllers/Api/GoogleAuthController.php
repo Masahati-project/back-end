0120901fe4b0005eb84b3ec21df065fb61293090
mohannad <mohannadjarad6@gmail.com>
2026-09-06 13:19:25 +0300

    edit google auth

--- app/Http/Controllers/Api/GoogleAuthController.php
+++ app/Http/Controllers/Api/GoogleAuthController.php
@@ -21,7 +21,7 @@ class GoogleAuthController extends Controller
         $client = new Client(['client_id' => config('services.google.client_id')]);
         $payload = $client->verifyIdToken($request->id_token);
 
-        if(!$payload) {
+        if (!$payload) {
             return response()->json([
                 'message' => 'Google token غير صالح'
             ], 401);
@@ -32,13 +32,22 @@ class GoogleAuthController extends Controller
         $name = $payload['name'] ?? $email;
         $avatar =  $payload['picture'] ?? null;
 
-        if($avatar) {
-            $profile_picture_url = $avatar->store('profile-pictures', 'public');
+        if ($avatar) {
+            $profile_picture_url = $avatar->store('profile-pictures', 'cloudinary');
         }
 
         $user = User::where('googleId', $googleId)->orWhere('email', $email)->first();
 
-        if(!$user) {
+        if ($user) {
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json([
+                'message' => 'تم تسجيل الدخول بنجاح',
+                'name' => $user->full_name,
+                'token' => $token,
+            ]);
+        }
+
+        if (!$user) {
             $user = User::create([
                 'full_name' => $name,
                 'email' => $email,
@@ -49,6 +58,18 @@ class GoogleAuthController extends Controller
                 'role' => $request->role,
                 'email_verified_at' => now()
             ]);
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'تم تسجيل الدخول بنجاح',
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->full_name,
+                    'role' => $user->role
+                ],
+                'token' => $token,
+            ]);
         } elseif (!$user->google_id) {
             // مستخدم مسجل بالإيميل العادي، بس أول مرة يستخدم قوقل
             $user->update([
@@ -56,14 +77,14 @@ class GoogleAuthController extends Controller
                 'provider' => 'google',
                 'profile_picture_url' => $user->profile_picture_url ?? $profile_picture_url,
             ]);
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'تم تسجيل الدخول بنجاح',
+                'name' => $user->full_name,
+                'token' => $token,
+            ]);
         }
-
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
-            'message' => 'تم تسجيل الدخول بنجاح',
-            'user' => $user,
-            'token' => $token,
-        ]);
     }
 }
